39f57b0182acd4c54231ca547a129a523dd7be35
Ge.Yaoyao <2529388917@qq.com>
2019-09-23 17:24:59 +0800

    第九次提交 2019/09/23

---
+++ 20190923/materialtest/Fruit.java
@@ -0,0 +1,21 @@
+package com.example.materialtest;
+
+public class Fruit {
+
+    private String name;
+
+    private int imaged;
+
+    public Fruit(String name,int imaged) {
+        this.name = name;
+        this.imaged = imaged;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getImaged()  {
+        return imaged;
+    }
+}
---
+++ 20190923/materialtest/MainActivity.java
@@ -0,0 +1,162 @@
+package com.example.materialtest;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+import android.widget.Toolbar;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.navigation.NavigationView;
+import com.google.android.material.snackbar.Snackbar;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout mDrawerLayout;
+
+    private SwipeRefreshLayout swipeRefresh;
+
+    private Fruit[] fruits = (new Fruit("Apple",R.drawable.apple)),
+    new Fruit("Banana",R.drawable.banana),
+        new Fruit("Orange",R.drawable.orange), new fruit("Watermelon",R.
+            drawable.watermelon),
+         new Fruit("Pear",R.drawable.pear), new fruit("Grape",R.
+            drawable.grape),
+         new Fruit("Pineapple",R.drawable.pineapple), new fruit("Strawberry",R.
+            drawable.strawberry),
+
+    private List<Fruit>  fruitList = new ArrayList<>();
+
+    private FruitAdapter adapter;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        swipeRefresh = (SwipeRefreshLayout) findViewById(R.id.swipe_refresh);
+        swipeRefresh.setColorSchemeResources(R.color.colorPrimary);
+        swipeRefresh.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                refreshFruits();
+            }
+        });
+    }
+    private void refreshFruits()  {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Tread.sleep(2000);
+
+                }catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        initFruits();
+                        adapter.notifyDataSetChanged();
+                        swipeRefresh.setRefreshing(false);
+                    }
+                });
+            }
+        }).start();
+        Toolbar toolbar =  (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        initFruits();
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        GridLayoutManager layoutManager = new GridLayoutManager(this,2);
+        recyclerView.setLayoutManager(layoutManager);
+        adapter = new FruitAdapter(fruitList);
+        recyclerView.setAdapter(adapter);
+       mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        NavigationView navView = (NavigationView) findViewById(R.id.nav_view);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Snackbar.make(View,"Data deleted",Snackbar.LENGTH_SHORT)
+                        .setAction("Undo", new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                Toast.makeText(MainActivity.this,"Data restored",
+                                        Toast.LENGTH_SHORT).show();
+                            }
+                        })
+                        .show();
+                Toast.makeText(MainActivity.this, "",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null)  {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_menu);
+        }
+
+        navView.setCheckedItem(R.id.nav_call);
+        navView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                mDrawerLayout.closeDrawers();
+                return true;
+            }
+        });
+    }
+
+
+    public boolean onCreateOptionsMenu(Menu menu)  {
+        getMenuInflater().inflate(R.menu.toolbar, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                break;
+            case R.id.backup:
+                Toast.makeText(this, "You clicked Backup", Toast.LENGTH_SHORT)
+                        .show();
+                break;
+
+            case R.id.delete:
+                Toast.makeText(this, "You clicked Delete", Toast.LENGTH_SHORT)
+                        .show();
+                break;
+            case R.id.settings:
+                Toast.makeText(this, "You clicked Settings", Toast.LENGTH_SHORT).
+                        show();
+                break;
+            default:
+        }
+        return true;
+    }
+
+    private void initFruits()  {
+        fruitList.clear();
+        for (int i = 0; i <  50; i++) {
+            Random random = new Random();
+            int index = random.nextInt(fruits.length);
+            fruitList.add(fruits[index]);
+        }
+    }
+}
